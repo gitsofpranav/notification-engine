01f93bd7d8ca12d5a226e4e78b4dc9d796267bab
gitsofpranav <dewanganpranav2@gmail.com>
2025-09-10 15:32:58 +0530

    NotificationSystem

---
+++ NotificationSystem.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+interface INotification{
+   String getContent();
+}
+
+
+public class NotificationSystem  {
+    public static void main(String[] args) {
+        
+    }
+}
